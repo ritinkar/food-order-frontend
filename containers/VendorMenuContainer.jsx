5574f2a325cadb59f3878745dea271831fb8a27e
ritinkar <ritinkar@outlook.com>
2017-10-28 17:33:12 +0530

    temp commit

--- containers/VendorMenuContainer.jsx
+++ containers/VendorMenuContainer.jsx
@@ -4,7 +4,7 @@ import VendorMenu from '../components/VendorMenu';
 
 
 //dummy variable please ignore
-const vl=[{name:"randall",price:3,id:4},{name:"akira",price:7,id:5},{name:"spiderman",price:8,id:7}]
+const vl=[{name:"paneer",price:140,id:4},{name:"chicken",price:150,id:5},{name:"aloo",price:100,id:7}]
 
 
 class VendorMenuContainer extends Component {
